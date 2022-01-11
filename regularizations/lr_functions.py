d5946541cf155d3553d95c0fc59d050ff2c0d212
talDrill <tal@drill-d.co.il>
2022-01-11 17:49:53 +0200

    delete

--- regularizations/lr_functions.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-def create_scheduler(optimizer, scheduler_method : str):
-    if scheduler_method == 'exponential':
-        return torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
--- system_utils/memory.py
+++
@@ -1,4 +0,0 @@
-import ctypes
-
-def malloc_trim():
-    ctypes.CDLL('libc.so.6').malloc_trim(0) 
